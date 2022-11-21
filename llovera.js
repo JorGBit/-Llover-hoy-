106e57cad2d6cf6b9549075ff2be565bb5c73b86
JorGBit <113969001+JorGBit@users.noreply.github.com>
2022-11-21 23:20:53 +0100

    Update llovera.js
    entrega final

--- llovera.js
+++ llovera.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const button = document.querySelector("button");
 const section = document.querySelector("section");
 const lloverHoy = document.getElementById("city");
@@ -12,15 +13,14 @@ console.log(now.getHours());
   if(now.getHours()>=7 && now.getHours()<=14){
     container.classList.remove("night");
     container.classList.add("morning");
-    
-  }
+    }
   if(now.getHours()>=15 && now.getHours()<=19){
 
     container.classList.remove("morning");
     container.classList.add("after");
    
   }
-  if(now.getHours()>=20 && now.getHours()<=6){
+  if(now.getHours()>=20){
  
     container.classList.remove("after");
     container.classList.add("night");
@@ -45,6 +45,7 @@ button.addEventListener("click", () => {
     function error(err) {
       alert(`¡Para decirte el tiempo, necesitamos conocer tu geolocalización!`);
     }
+
     navigator.geolocation.getCurrentPosition(success, error, options);
     navigator.geolocation.getCurrentPosition((posicion) => {
       latitud = posicion.coords.latitude;
@@ -56,28 +57,31 @@ button.addEventListener("click", () => {
           return response.json();
         })
         .then((data) => {
-          // console.log(data);
+          console.log(data);
           lloverHoy.textContent = `Usted se encuentra en: ${data.locality.name}`
           
           for (let y = 1; y <= 8; y++) {
             const siLLueve = data.hour_hour[`hour${y}`].text;
+
             if (
+              siLLueve === "Muy nuboso con lluvias" ||
               siLLueve === "Cubierto con lluvias" ||
               siLLueve === "Parcilmente nuboso con lluvias" ||
-              siLLueve === "Muy nuboso con lluvias"
+              siLLueve === "Parcilamente nuboso con lluvia"
             ) {
               const h1 = document.createElement("h1");
               h1.textContent = "En las próximas 8h lloverá";
-              body.append(h1);
+              main.append(h1);
               break;
-            } else {
+            }
+            if (siLLueve === data.hour_hour[`hour${8}`].text) {
               const h1 = document.createElement("h1");
               h1.textContent = "En las próximas 8h no lloverá";
               main.append(h1);
               break;
             }
           }
-
+ 
 
           for (let i = 1; i <= 8; i++) {
             const { humidity, date, temperature, hour_data, text } =
@@ -89,15 +93,18 @@ button.addEventListener("click", () => {
             const icono = document.createElement("img");
             const horaP = document.createElement("p");
             const dateP = document.createElement("p");
+
+
             h2.textContent = text;
             temperatureP.textContent = `${temperature}ºC`;
             humidityP.textContent = `${humidity}% Hum`;
             horaP.textContent = hour_data;
             dateP.textContent = date;
-            switch (text) {
+
+          switch (text) {
               case `Despejado`:
                 icono.src = "/animated/day.svg";
-                break;
+               break;
               case `Cubierto`:
               case `Parcialmente nuboso`:
                 icono.src = "/animated/cloudy-day-1.svg";
@@ -111,23 +118,18 @@ button.addEventListener("click", () => {
                 icono.src = "/animated/rainy-5.svg";
                 break;
               case "Muy nuboso con lluvias":
-                icono.src = "/animated/thunder.svg";
+              case "Muy nuboso con lluvia":
+              case "Cubierto con lluvias":
+                icono.src = "/animated/rainy-7.svg";
                 break;
-            }
-            article.append(h2, temperatureP, icono, humidityP, horaP, dateP);
-            section.append(article);
-            
-            const displayBackgroundImage =(data)=>{
-             dayHour=hour_data;
-             if (dayHour>6&&dayHour<18){
-              Container.classList.remove("night");
-              Container.classList.add("day")
-             }
-             else {
-              container.classList.remove("day");
-              container.classList.add("night");
-             }
-            }
+              case "Cubierto con tormentas":
+                icono.src = "/animated/thunder.svg";
+              }
+           
+              article.append(h2, temperatureP, icono, humidityP, horaP, dateP);
+              section.append(article);
+                     
+          
           }
 
         })
